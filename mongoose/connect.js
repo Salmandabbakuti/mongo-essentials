b0fecc0f77bf20a26c4d7475a08e75f501e3e7f2
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-03-15 19:44:49 +0530

    Update connect.js

--- mongoose/connect.js
+++ mongoose/connect.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://salmandabbakuti:9F5mTuykkDzKUF4S@dev-f0v8q.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true },);
+mongoose.connect('<mongodb url>', { useUnifiedTopology: true, useNewUrlParser: true },);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
