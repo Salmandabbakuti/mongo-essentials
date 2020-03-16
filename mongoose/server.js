604892b19b4cfb4b1e30e1d77bb9797b3dee4a81
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-03-16 11:59:48 +0530

    Update server.js
    document not found alert

--- mongoose/server.js
+++ mongoose/server.js
@@ -20,8 +20,7 @@ var Product = mongoose.model('products',productSchema );
 app.get('/api/read/', async function (req, res) {
     
 try{
-Product.find(req.query, function (error, documents) { // if no query passed, it will return all docs in collection
-  console.log(documents);
+  Product.find(req.query, function (error, documents) { // if no query passed, it will return all docs in collection
   res.send(documents)
   });
 }catch (err) {
@@ -45,9 +44,11 @@ try{
 let query = {}
 query.name = req.body.name
 let isExists = await Product.updateOne(query, req.body,{findAndModify:false})
-if (!isExists) res.status(404).send("No item found")
-await Product.save()
-res.send("Document Updated..")
+if (!isExists.n==1){
+  res.status(404).send("No item found")
+  return
+   }
+  res.send("Document Updated..")
 }catch (err) {
     res.status(500).send(err)
   }    
@@ -57,8 +58,11 @@ app.delete('/api/delete/', async function (req, res) {
     
 try{
 let isExists = await Product.deleteOne(req.body)
-if (!isExists) res.status(404).send("No item found")
-    res.status(200).send("Deleted food Data Successfully.")
+if (!isExists.n==1) {
+  res.status(404).send("No item found") //n value will be 1 if item found and updated
+  return
+}
+res.status(200).send("Deleted Document Successfully.")
 }catch (err) {
     res.status(500).send(err)
   }    
